10e3150c694912aa8dd48c688462e656902931b9
gtchappy <2425172709@qq.com>
2023-07-15 17:04:23 +0800

    重构：继续精简代码

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,23 +1,19 @@
-import {defineComponent} from "vue";
 import s from './WelcomeLayout.module.scss'
 import pig from '../../assets/icons/pig.svg'
 import {RouterLink} from "vue-router";
 import {WelcomeLayout} from "./WelcomeLayout";
 
-export const First = defineComponent({
-
-    setup: () => {
-        const slots = {
-            icon:()=><img src={pig} alt={"猪"}></img>,
-            title:()=><h2>会挣钱<br/>还会省钱</h2>,
-            buttons:()=><>
-                <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-                <RouterLink to="/welcome/2">下一页</RouterLink>
-                <RouterLink to="/start">跳过</RouterLink>
-            </>
-        }
-        return () => (
-          <WelcomeLayout v-slots = {slots}></WelcomeLayout>
-        )
-    }
-})
+export const First = () =>
+    (
+        <WelcomeLayout>
+            {{
+                icon: () => <img src={pig} alt={"猪"}></img>,
+                title: () => <h2>会挣钱<br/>还会省钱</h2>,
+                buttons: () => <>
+                    <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+                    <RouterLink to="/welcome/2">下一页</RouterLink>
+                    <RouterLink to="/start">跳过</RouterLink>
+                </>
+            }}
+        </WelcomeLayout>
+    )
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,24 +1,19 @@
-import {defineComponent} from "vue";
 import s from './WelcomeLayout.module.scss'
 import chart from '../../assets/icons/chart.svg'
 import {RouterLink} from "vue-router";
 import {WelcomeLayout} from "./WelcomeLayout";
 
-export const Third = defineComponent({
+export const Third=() => (
+    <WelcomeLayout>
+        {{
+            icon: () => <img class={s.icon} src={chart} alt={"图标"}/>,
+            title: () => <h2>每日提醒<br/>不遗漏每一笔账单</h2>,
+            buttons: () => <>
+                <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+                <RouterLink to="/welcome/4">下一页</RouterLink>
+                <RouterLink to="/start">跳过</RouterLink>
+            </>
+        }}
+    </WelcomeLayout>
+)
 
-    setup: () => {
-        return () => (
-            <WelcomeLayout>
-                {{
-                    icon: () => <img class={s.icon} src={chart} alt={"图标"}/>,
-                    title: () => <h2>每日提醒<br/>不遗漏每一笔账单</h2>,
-                    buttons: () => <>
-                        <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-                        <RouterLink to="/welcome/4">下一页</RouterLink>
-                        <RouterLink to="/start">跳过</RouterLink>
-                    </>
-                }}
-            </WelcomeLayout>
-        )
-    }
-})
